6f47ae91dcb7d2ad1540efbf0b4c0450d6acf438
REZAN <asifrezan@gmail.com>
2021-10-27 02:44:33 +0600

    eightth commit

--- E_Shop/settings.py
+++ E_Shop/settings.py
@@ -124,11 +124,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "uoloads"),
+    os.path.join(BASE_DIR, "products/static/products")
+    
+]
 
 #MEDIA_ROOT= BASE_DIR/ 'uploads'
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'uploads')
+
+
 
 #MEDIA_URL= '/files/'
 MEDIA_URL= '/uploads/'
